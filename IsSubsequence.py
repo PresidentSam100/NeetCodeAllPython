61e17d0d4bf2e7e1a35606ab24481b380146fc76
Sam Lu <pbcmarsh@gmail.com>
2024-07-06 23:02:55 -0700

    Create IsSubsequence.py

---
+++ IsSubsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sIndex = 0
+        tIndex = 0
+        while sIndex < len(s) and tIndex < len(t):
+            if s[sIndex] == t[tIndex]:
+                sIndex += 1
+            tIndex += 1
+        return sIndex == len(s)
