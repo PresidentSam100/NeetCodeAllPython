8fd1fd2f6de8893915ec66538504cff41329a6ba
Sam Lu <pbcmarsh@gmail.com>
2024-07-06 22:14:21 -0700

    Create ConcatenationOfArray.py

---
+++ ConcatenationOfArray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        concat = []
+        for num in nums:
+            concat.append(num)
+        for num in nums:
+            concat.append(num)
+        return concat
